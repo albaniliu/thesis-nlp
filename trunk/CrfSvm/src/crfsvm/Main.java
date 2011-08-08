cb8a0dd7abb3bbf77a3c21e754a02d8074ef85f5
anhdungle1986@gmail.com <anhdungle1986@gmail.com@19d674ed-bab9-c4f4-305c-9ea7e9f0e6fa>
2011-08-08 17:04:52 +0000

    sdf

--- trunk/CrfSvm/src/crfsvm/Main.java
+++ trunk/CrfSvm/src/crfsvm/Main.java
@@ -4,11 +4,58 @@
  */
 package crfsvm;
 
+import crfsvm.crf.een_phuong.IOB2Converter;
+import crfsvm.crf.een_phuong.TaggingTrainData;
+import crfsvm.util.Document;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author banhbaochay
  */
 public class Main {
+    
+    public void mergeFile() {
+        File dir = new File("data/dataToRetrain/entity");
+        File[] fileList = dir.listFiles(new FilenameFilter() {
+
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".txt");
+            }
+        });
+        Document retDoc = new Document();
+        for (File file : fileList) {
+            Document doc = new Document(file);
+            retDoc.append(doc);
+        }// end foreach file
+        retDoc.print2File("data/Temp/merge.txt");
+    }// end mergeFile method
+    
+    public void createIOB2() {
+        String[] args = new String[2];
+        args[0] = "data/Temp/merge.txt";
+        args[1] = "data/Temp/iob2.txt";
+        IOB2Converter.main(args);
+    }// end createIOB2 method
+    
+    public void taggingTrain() {
+        String[] args = new String[3];
+        args[0] = "data/Temp/iob2.txt";
+        args[1] = "data/Temp/iob2-tagged.txt";
+        args[2] = "model";
+        TaggingTrainData.main(args);
+    }// end taggingTrain method
+    
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.createIOB2();
+        m.taggingTrain();
+    }// end main class
     
 }// end Main class
 
--- trunk/CrfSvm/src/crfsvm/util/Document.java
+++ trunk/CrfSvm/src/crfsvm/util/Document.java
@@ -8,6 +8,8 @@ import crfsvm.svm.org.itc.irst.tcc.sre.data.ReadWriteFile;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -20,25 +22,90 @@ import java.util.logging.Logger;
  */
 public class Document {
 
+    /**
+     * Constructor empty
+     */
     public Document() {
         lineList = new ArrayList<String>();
     }
 
+    /**
+     * Tao Document tu duong dan 1 file. Doc file bang UTF-8
+     * @param filePath 
+     */
+    public Document(String filePath) {
+        this(new File(filePath));
+    }
+
+    /**
+     * Tao Document tu 1 file. Doc file bang UTF-8
+     * @param file 
+     */
     public Document(File file) {
+        filePath = file.getAbsolutePath();
         lineList = new ArrayList<String>();
         try {
             BufferedReader in = ReadWriteFile.readFile(file, "UTF-8");
             String line = null;
-            int i = 0;
             while ((line = in.readLine()) != null) {
                 if (!line.equals("")) {
                     lineList.add(line);
-                }// end if line.equals("")
+                }// end if !line.equals("")
             }
         } catch (Exception ex) {
             Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    @Override
+    protected Document clone() {
+        Document doc = new Document();
+        doc.setFilePath(getFilePath());
+        doc.lineList = new ArrayList<String>(this.lineList);
+        return doc;
+    }
+    
+    /**
+     * Noi cac document vao 1 document co truoc. 
+     * Document ban dau bi thay doi
+     * @param docs
+     */
+    public void append(Document... docs) {
+        if (docs.length > 0) {
+            for (Document document : docs) {
+                for (String line : document.lineList) {
+                    add(line);
+                }// end foreach line
+            }// end foreach document
+        }// end if docs.length > 0
+    }// end append method
+    
+    /**
+     * In document ra file. Cac dong trong file phan tach boi 1 dong trang
+     * @param file 
+     */
+    public void print2File(File file) {
+        try {
+            PrintWriter out = ReadWriteFile.writeFile(file, "UTF-8");
+            for (String line : lineList) {
+                out.print(line);
+                out.print("\n\r");
+            }// end foreach line
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(Document.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }// end print2File method
+    
+    /**
+     * In document ra file. Cac dong trong file phan tach boi 1 dong trang
+     * @param fileName 
+     */
+    public void print2File(String fileName) {
+        print2File(new File(fileName));
+    }// end print2File method
     
     /**
      * 
@@ -75,13 +142,41 @@ public class Document {
         return list;
     }// end createBagging method
     
+    private String filePath;
     private List<String> lineList;
     
     public static void main(String[] args) {
         File f = new File("data/dataToRetrain/entity/Dung1.txt");
         Document doc = new Document(f);
-        List list = doc.createBagging(2, 30);
+        List list = doc.createBagging(2, 5);
+        String path = "data/Temp/";
+        for (int i = 0; i < list.size(); i++) {
+            File bag = new File(path + "bag" + i + ".txt");
+            Document bagDoc = (Document) list.get(i);
+            bagDoc.print2File(bag);
+        }// end for i
+        File bag0 = new File("data/Temp/bag0.txt");
+        File bag1 = new File("data/Temp/bag1.txt");
+        Document doc0 = new Document(bag0);
+        Document doc1 = new Document(bag1);
+        Document retDoc = new Document();
+        retDoc.append(doc0, doc1);
+        retDoc.print2File("data/Temp/bag2.txt");
         
     }// end main class
+
+    /**
+     * @return the filePath
+     */
+    public String getFilePath() {
+        return filePath;
+    }
+
+    /**
+     * @param filePath the filePath to set
+     */
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
 }// end Document class
 
